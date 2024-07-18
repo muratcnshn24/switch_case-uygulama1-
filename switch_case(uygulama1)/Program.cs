7ddaa2d27263184834cbfd77497140ce02f370e4
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 21:39:51 +0300

    Proje dosyası ekle.

---
+++ switch_case(uygulama1)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case_uygulama1_
+{
+    class Program
+    {
+        public enum Renkler {Kirimizi, Yesil, Mavi} // değer vermezsek numaralandırmaya sıfırdan başlarlar.
+
+        static void Main(string[] args)
+        {
+            Renkler r = (Renkler)(new Random()).Next(0,3);
+
+            switch (r)  // r yazdığımda switch case otomatik olara buraya yazılıyor.
+            {
+                case Renkler.Kirimizi:
+                    Console.WriteLine(" Renk Kirmizidir");
+                    break;
+                case Renkler.Yesil:
+                    Console.WriteLine(" Renk Yeşildir");
+                    break; // bütün komutları bir birinde nayırmak için break komutu zorunlu
+                case Renkler.Mavi:
+                    Console.WriteLine(" Renk Mavidir");
+                    break;
+                default:
+                    Console.WriteLine(" Renk Bilinmiyor");
+                    break;
+            }
+        }
+
+
+ 
+    }
+}
